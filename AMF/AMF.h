4aea5f6515c1f83bf228245827a0e12ca256dd7f
Max <mczonk@gmail.com>
2014-03-18 09:46:10 +0100

    Initial Commit

---
+++ AMF/AMF.h
@@ -0,0 +1,13 @@
+//
+//  AMF.h
+//  AMF
+//
+//  Created by Maximilian Christ on 18/03/14.
+//  Copyright (c) 2014 Boinx Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AMF : NSObject
+
+@end
---
+++ AMF/AMF.m
@@ -0,0 +1,13 @@
+//
+//  AMF.m
+//  AMF
+//
+//  Created by Maximilian Christ on 18/03/14.
+//  Copyright (c) 2014 Boinx Software. All rights reserved.
+//
+
+#import "AMF.h"
+
+@implementation AMF
+
+@end
